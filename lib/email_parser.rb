cd51c5bbb453e7452b6f16d07805daf4f00e7906
Joshua McArthur <jcmcarth06@gmail.com>
2020-05-17 00:51:09 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -2,14 +2,19 @@
 class EmailAddressParser
   attr_accessor :name, :csv_emails
 
+  #establishes name and csv_emails attributes to be accessed throughout the entire class method
+
   def initialize(csv_emails)
     @csv_emails = csv_emails
   end
+  #initializes csv_emails variable 
 
   def parse
     csv_emails.split.collect do |address|
       address.split(',')
+      #collects and parses emails array split by commas
     end
     .flatten.uniq
+    #gets rid of duplicates
   end
 end
